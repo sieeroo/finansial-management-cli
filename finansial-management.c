37b63f8df1ff312fd8b28be18e908e0a78e1d3c4
Rangga Budi Satria <ranggasatria.dev@gmail.com>
2026-01-25 19:32:35 +0700

    Add data user feature

--- finansial-management.c
+++ finansial-management.c
@@ -5,6 +5,7 @@
 
 void save_data();
 void login_user();
+void main_menu();
 void set_persentase();
 void count_budget_page();
 void finansial_report_page();
@@ -15,16 +16,63 @@ typedef struct {
     int password;
 } User;
 
-User user = {"", '[4]'};
+User user = {"", 4};
 
 void save_data() {
-
+    FILE *f;
+    f = fopen("savedata.txt", "r");
 }
 
 void login_user() {
 
+    char choice;
+    
+    while(1) {
+        printf("Ingin memuat data lama? (y/n) : ");
+        scanf("%c", &choice);
+
+        if(choice == 'y' || choice == 'Y' || choice == 'n' || choice =='N') {
+            break;
+        }
+        else {
+            printf("Input tidak valid! Silahkan masukkan 'y' atau 'n'. \n");
+        }
+        getchar();
+    }
+
+    if(choice == 'y' || choice == 'Y') {
+        FILE *f = fopen("savedata.txt", "r");
+        if(f == NULL) {
+            printf("Tidak ada data tersimpan\n");
+            printf("Silahkan membuat akun\n");
+        }
+        else {
+            
+        }
+    }
+
+    printf("Masukkan Username : ");
+    fgets(user.username, sizeof(user.username), stdin);
+
+    FILE *f = fopen("savedata.txt", "r");
+    if(f == NULL) {
+        printf("Membuat data baru\n");
+    }
+    else {
+        printf("Selamat datang kembali\n");
+    }
 }
 
+void main_menu() {
+    printf("== Menu Utama ==\n");
+    printf("\n");
+    printf("1. Edit Persentase\n");
+    printf("2. Atur Keuangan\n");
+    printf("3. Lihat Tabel Keuangan\n");
+    printf("4. Lihat Aktivitas\n");
+    printf("5. Keluar\n");
+
+}
 void set_persentase() {
 
 }
@@ -50,6 +98,9 @@ void welcome_page() {
 }
 
 int main() {
+
+    welcome_page();
+    login_user();
 
     return 0;
 }
